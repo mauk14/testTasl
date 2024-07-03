e0a2c71d58b0ee8801d9e0d30a5ba341570b6784
mauk14 <mnd33599@gmail.com>
2024-07-03 13:38:30 +0500

    first commit

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"newProject/internal/delivery/http"
 	"newProject/internal/service"
+	"os"
 )
 
 func main() {
 	app := http.NewApp(service.New("https://7103.api.greenapi.com"))
-	err := app.Route().Run(":4000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":4000"
+	}
+	err := app.Route().Run(port)
 	if err != nil {
 		log.Fatal(err)
 		return
